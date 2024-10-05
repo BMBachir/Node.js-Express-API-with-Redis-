c054bef1cc01014ac9b556623ea28c7f68aca1d7
BMBachir <boubaidjabachir3@gmail.com>
2024-10-05 20:10:40 +0100

    creat simple endpoint fetch photos

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Back-end is running..!");
+});
+
+app.get("/photos", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos"
+    );
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data" });
+  }
 });
 
 app.listen(PORT, () => {
